3cc0098d296322c9f87f749ec151493f317d5e5e
mohamadreza62-arch <mohamad.reza.62@gmail.com>
2026-09-25 09:57:17 +0330

    Update MainActivity.kt

--- app/src/main/java/com/example/privatebrowser/MainActivity.kt
+++ app/src/main/java/com/example/privatebrowser/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.privatebrowser
 
 import android.annotation.SuppressLint
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
